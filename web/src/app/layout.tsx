784d74a230b8266c1ccf3f51cb7cf1f630fd51b3
dev1siWoo <102544941+dev1siWoo@users.noreply.github.com>
2024-04-14 00:16:58 +0300

    Feature/not found (#3)
    * feat(): add graphql code
    
    * feat(): add enc
    
    * feat(): add not-found page, error and add nextTopLoader

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,13 +1,14 @@
-import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter as FontSans } from "next/font/google"
+import "./globals.css"
 
-import { cn } from "@/shared/lib/utils";
+import { cn } from "@/shared/lib/utils"
+import NextTopLoader from "nextjs-toploader"
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+	subsets: ["latin"],
+	variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Titan Advertisiment",
@@ -15,20 +16,25 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body
+				className={cn(
+					"min-h-screen bg-background font-sans antialiased",
+					fontSans.variable
+				)}
+			>
+				<NextTopLoader
+					color='#00AA00'
+					initialPosition={0.6}
+					showSpinner={false}
+				/>
+				{children}
+			</body>
+		</html>
+	)
 }
