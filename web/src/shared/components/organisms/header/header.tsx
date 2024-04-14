7bbb2b631f304eedd43238d18868ae54eb400d93
Sh1wOo <49766681+Sh1wOo@users.noreply.github.com>
2024-04-15 02:29:08 +0300

    feat(): created simple header with use mobile hook

---
+++ web/src/shared/components/organisms/header/header.tsx
@@ -0,0 +1,30 @@
+'use client'
+import useIsMobile from "@/shared/hooks/is-mobile-hook";
+import Link from "next/link";
+import React from "react";
+import TabsMobile from "./tabs-mobile";
+import { ModeToggle } from "../../maleculas/mode-toggle";
+
+const Header = () => {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return <TabsMobile/>
+  }
+  
+
+  return (
+    <header className="w-[95%] h-[3rem] m-auto flex justify-between items-center">
+      <div>
+        <Link href="/">Titan Project</Link>
+      </div>
+      <nav className="flex gap-3 items-center">
+        <Link href="/sign-in">Sign In</Link>
+        <Link href="/sign-up">Sign up</Link>
+        <ModeToggle/>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ web/src/shared/components/organisms/header/tabs-mobile.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TabsMobile = () => {
+  return (
+    <div>Tabs Mobile</div>
+  )
+}
+
+export default TabsMobile
