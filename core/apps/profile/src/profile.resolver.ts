e60070987e480cd89fa33a60b1d5e42f3dd1eca6
Sh1wOo <49766681+Sh1wOo@users.noreply.github.com>
2024-04-15 15:36:05 +0300

    feat(): profile search (#6)

--- core/apps/profile/src/profile.resolver.ts
+++ core/apps/profile/src/profile.resolver.ts
@@ -1,16 +1,26 @@
 import { Args, Query, Resolver } from "@nestjs/graphql";
 import { ProfileService } from "./profile.service";
-import { UserProfile } from "./entities/profile.entitie";
+import { UserProfile, UserProfileSearch } from "./entities/profile.entitie";
 import { ProfileDTO } from "./dto/profile.dto";
 import { UseGuards } from "@nestjs/common";
 
 @Resolver("Profile")
 export class ProfileResolver {
-  constructor(private profileServie: ProfileService) {}
+  constructor(private profileService: ProfileService) {}
 
   @Query(() => UserProfile)
   async profile(@Args("userName") userName: string) {
-    return await this.profileServie.getProfile(userName);
+    return await this.profileService.getProfile(userName);
+  }
+
+  @Query(() => UserProfileSearch)
+  async searchProfile(
+    @Args("userName") userName: string,
+    @Args("limit", { defaultValue: 5 }) limit?: number
+  ) {
+    const { users, isPublic } = await this.profileService.searchUserProfile(userName, limit);
+    const publicUsers = users.filter((_, index) => isPublic[index]);
+    return { users: publicUsers, isPublic: isPublic };
   }
 
   // @UseGuards(AuthGuard)
