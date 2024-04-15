34f36e3958cb56d4e381ecc4c346752ece8d0f39
Sh1wOo <49766681+Sh1wOo@users.noreply.github.com>
2024-04-15 15:36:42 +0300

    feat(): profile search (#6) (#7)

--- core/apps/profile/src/profile.service.ts
+++ core/apps/profile/src/profile.service.ts
@@ -1,27 +1,26 @@
-import { BadGatewayException, Injectable} from '@nestjs/common';
-import { PrismaService } from '../../../prisma/prisma.service';
-import { UserProfile } from './entities/profile.entitie';
+import { BadGatewayException, Injectable } from "@nestjs/common";
+import { PrismaService } from "../../../prisma/prisma.service";
+import { Profile, UserProfile } from "./entities/profile.entitie";
 
 @Injectable()
 export class ProfileService {
   constructor(private prisma: PrismaService) {}
-  
+
   async getProfile(userId: string): Promise<UserProfile> {
     const user = await this.prisma.user.findUnique({
       where: {
-        id: userId
-      }
+        id: userId,
+      },
     });
-    
 
-    if(!user) {
-      throw new BadGatewayException('User not found');
+    if (!user) {
+      throw new BadGatewayException("User not found");
     }
 
     const profile = await this.prisma.profile.findUnique({
       where: {
-        userId: user.id  
-      }
+        userId: user.id,
+      },
     });
 
     return {
@@ -31,9 +30,30 @@ export class ProfileService {
     };
   }
 
-  async getSettings(userName: string) {
-    
-  }
+  async searchUserProfile(userName: string, limit?: number): Promise<{ users: Profile[]; isPublic: boolean[] | null }> {
+    const profiles = await this.prisma.profile.findMany({
+      where: {
+        user: {
+          name: {
+            contains: userName,
+            mode: 'insensitive',
+          },
+        },
+        isPublic: true,
+      },
+      take: limit || 5,
+      include: {
+        user: true,
+      },
+    });
   
+    return {
+      users: profiles.map((profile) => profile.user),
+      isPublic: profiles.map((profile) => profile.isPublic),
+    };
+  }
+
+  async getSettings(userName: string) {}
+
   async updateSettings(settings) {}
 }
