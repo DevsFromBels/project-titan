5429138885a0cd0f0cfd393c7176df4ff68dbed5
Sh1wOo <49766681+Sh1wOo@users.noreply.github.com>
2024-05-11 06:22:18 +0300

    chore(): profile fixed server error code 500

--- web/src/app/[locale]/(profile)/layout.tsx
+++ web/src/app/[locale]/(profile)/layout.tsx
@@ -1,15 +1,23 @@
 import { ApolloWrapperServer } from "@/features/graphql/server/apollo-wrapper-server";
+import { i18nLocales } from "@/shared/constants/i18n-locales";
+import { unstable_setRequestLocale } from "next-intl/server";
+
+export function generateStaticParams() {
+  return i18nLocales.map((locale) => ({ locale }));
+}
 
 export default function ProfileLayout({
   children,
+  params: { locale },
 }: {
   children: React.ReactNode;
+  params: { locale: string };
 }) {
+  unstable_setRequestLocale(locale);
+
   return (
-      <ApolloWrapperServer>
-        <main className="flex h-[100vh] w-[100vw]">
-          {children}
-        </main>
-      </ApolloWrapperServer>
+    <ApolloWrapperServer>
+      <main className="flex h-[100vh] w-[100vw]">{children}</main>
+    </ApolloWrapperServer>
   );
 }
