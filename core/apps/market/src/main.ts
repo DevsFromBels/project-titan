bcfbd911503d8c257b86693fa6769ef0dd2cb4d7
Sh1wOo <49766681+Sh1wOo@users.noreply.github.com>
2024-05-14 22:08:34 +0300

    feat(): del market

--- core/apps/market/src/main.ts
+++ core/apps/market/src/main.ts
@@ -1,16 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from "@nestjs/core";
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import multipart from '@fastify/multipart';
-import { MarketModule } from './market.module';
-import { join } from 'path';
+} from "@nestjs/platform-fastify";
+import multipart from "@fastify/multipart";
+import { MarketModule } from "./market.module";
+import { join } from "path";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     MarketModule,
-    new FastifyAdapter(),
+    new FastifyAdapter()
   );
   await app.register(multipart, {
     limits: {
@@ -19,10 +19,10 @@ async function bootstrap() {
   });
 
   app.useStaticAssets({
-    root: join(__dirname, '..', '..', 'uploads', 'market'), // Обновленный путь к папке с файлами
-    prefix: '/uploads/market',
+    root: join(__dirname, "..", "..", "uploads", "market"), // Обновленный путь к папке с файлами
+    prefix: "/uploads/market",
   });
-  
-  await app.listen(4003, '0.0.0.0');
+
+  await app.listen(4003, "0.0.0.0");
 }
 bootstrap();
