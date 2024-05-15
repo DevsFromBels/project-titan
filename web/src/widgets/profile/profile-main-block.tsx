22c94d0880c7646ceb22b67a094cf9c567f3008f
Sh1wOo <49766681+Sh1wOo@users.noreply.github.com>
2024-05-15 03:39:39 +0300

    feat(): client profile

--- web/src/widgets/profile/profile-main-block.tsx
+++ web/src/widgets/profile/profile-main-block.tsx
@@ -2,22 +2,23 @@ import Image from "next/image";
 import React from "react";
 
 interface IProfileMainBlockWidget {
-  id: string;
+  image: string;
   username: string;
 }
 
-const ProfileMainBlockWidget = ({id,  username }: IProfileMainBlockWidget) => {
-  const image =
-    id === "sh1woo"
-      ? "https://titanproject.top/sh1woo.jpeg"
-      : "https://titanproject.top/cat.jpeg";
+const ProfileMainBlockWidget = ({image,  username }: IProfileMainBlockWidget) => {
+  let porfileImage = 'https://avatars-api.titanproject.top' + image;
+
+  if(!image) {
+    porfileImage = "https://titanproject.top/cat.jpeg"
+  }
 
   return (
     <div className="relative w-screen lg:w-[calc(100vw_-_250px)] flex flex-col justify-center items-center h-[200px] gap-2 bg-[#222]">
       <div className="blur-[50px] w-[60px] h-[60px] rounded-full bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0"></div>
       <div className="relative flex items-start z-10 select-none">
         <Image
-          src={image}
+          src={porfileImage}
           className="rounded-full shadow-lg shadow-cyan-500/10 border border-slate-100"
           width={100}
           height={100}
