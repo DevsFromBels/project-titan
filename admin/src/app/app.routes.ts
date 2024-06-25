4679d2a7dfd8cecadb8c7de9f0ebb3145963d46f
Sh1wOo <49766681+Sh1wOo@users.noreply.github.com>
2024-06-25 06:03:22 +0300

    feat(): some fixes

--- admin/src/app/app.routes.ts
+++ admin/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { UsersComponent } from './users/users.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { ModerationComponent } from './moderation/moderation.component';
+import { UserIDComponent } from './user-id/user-id.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,10 @@ export const routes: Routes = [
   {
     path: 'users',
     component: UsersComponent,
+  },
+  {
+    path: 'user/:id',
+    component: UserIDComponent,
   },
   {
     path: 'moderation',
